ff3980789988f5d5cbe98ba68c92818773f5aac7
Aber <aberkih.m@gmail.com>
2018-03-04 13:43:41 +0100

    filter function

--- inc/functions.php
+++ inc/functions.php
@@ -1,24 +1,16 @@
 <?php
 
 function filter($table,$tab){
-
-    $tab2 = array();
-    
     foreach($table as $key => $value){
-        
-        
+        $tab2 = array();
         foreach($tab as $ref){
             $str = strtolower($ref[$key]);
             $substr = strtolower($value);
             if(strpos($str,$substr)!==false){
-                array_push($tab2,$ref);  
-                 
-            }
-            
+                array_push($tab2,$ref);      
+            }   
         }
-         
         $tab = $tab2;
-       
     }
     return $tab;
 }
