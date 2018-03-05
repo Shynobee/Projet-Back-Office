eb980e14517db7bb9d9a74f397fcfa994a0ad79d
Aber <aberkih.m@gmail.com>
2018-03-05 21:33:50 +0100

    content

--- inc/content.php
+++ inc/content.php
@@ -2,9 +2,6 @@
 include('functions.php');
 if (isset($_GET['id'])){$_SESSION['detail'][0]['lastview']=$_GET['id'];}
 $_SESSION['client'] = $mdb->request('SELECT * FROM refs ORDER BY `id` DESC  ', []);
-
-
-
 $detail = $mdb->request('SELECT * FROM refs WHERE id = :id', array('id' => $_GET['id']));
 
 
@@ -19,19 +16,10 @@ switch ($_GET['page']) {
     case 'CLIENT LIST':
         $listpage = array();
         $search = array();
-        if(!empty($_POST['searchclient'])){
-        $search['client']=$_POST['searchclient'];}
-        if(!empty($_POST['published'])){
-        $search['status']=$_POST['published'];}
-        if(!empty($_POST['postedby'])){
-        $search['createdBY']=$_POST['postedby'];}
-        
-            
-        if(!empty($search)){
-        $listpage1 = filter($search,$_SESSION['client'] );}
-        else{
-        $listpage1 = $_SESSION['client'];
-        }
+        if(!empty($_POST['searchclient'])){ $search['client']=$_POST['searchclient'];}
+        if(!empty($_POST['published'])){ $search['status']=$_POST['published'];}
+        if(!empty($_POST['postedby'])){ $search['createdBY']=$_POST['postedby'];}           
+        if(!empty($search)){ $listpage1 = filter($search,$_SESSION['client'] );}else{ $listpage1 = $_SESSION['client']; }
         for ($i = 0; $i < (count($listpage1)/14); $i++) { $ar=array_slice($listpage1, ($i*14), 14); array_push($listpage,$ar);}
         include 'client-list.php';
         break;
