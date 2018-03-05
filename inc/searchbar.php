98b68611a5c38e8b2de04570e0d5a67e5265e539
Aber <aberkih.m@gmail.com>
2018-03-05 18:24:45 +0100

    searchbar

--- inc/searchbar.php
+++ inc/searchbar.php
@@ -1,27 +1,20 @@
-<<<<<<< HEAD
-
 <form action="" method="post">
     <div class="search">
     <input type="search" name="searchclient" placeholder="Client">
     <i></i>
+    
     </div>
     <div class="filter">
-    <input list="Published">
-        <datalist id="Published">
-            <option value="Published">
-            <option value="Not published">
-            <option value="Draft">
-        </datalist> 
+        <select id="Published">
+            <option name='published' value="Published">Published</option>
+            <option name='published' value="Not published">Not published</option>
+            <option name='published' value="Draft">Draft</option>
+        </select> 
+        <label for="postedby">Posted By</label>
+        <input type="text" name="postedby">
+
     </div>
     <input type="submit" value="search">
 </form>
-=======
-<form action="" method="post">
-    <label for="client">Client</label>
-    <input type="checkbox" name="client" checked>
-    <label for="context">Context</label>
-    <input type="checkbox" name="context">
-    <input type="search" name="searchclient">
-    <input type="submit" value="Search">
-</form>
->>>>>>> 5c77a878b1b051d9c2b9695f8fd998b0bef08644
+
+
